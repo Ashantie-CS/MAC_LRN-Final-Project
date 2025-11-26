ee569235dbb4979f5a4e9d18acec52467564e30f
Ashantie-CS <ashantie.cs@gmail.com>
2025-11-26 15:22:50 +0800

    feat(training): Trained initial model using yolov8n

--- model training/train-model.py
+++ model training/train-model.py
@@ -1,9 +1,14 @@
 from ultralytics import YOLO
-import torch
-torch.cuda.empty_cache()
 
-# Load a model
-model = YOLO('yolov8n.yaml')
+def main():
+    model = YOLO("yolov8n.pt")
+    results = model.train(
+        data="../data/data.yaml",
+        epochs=60,
+        imgsz=416,
+        batch=16,
+        device=0,
+    )
 
-# Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data="..\data\data.yaml", epochs=100, imgsz=640, batch=16)
+if __name__ == "__main__":
+    main()
