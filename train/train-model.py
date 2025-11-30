1a9d3d8c1b6adb1a826718ef074a98b4c2164ab6
Ashantie Louize B. Demanalata <ashantie.cs@gmail.com>
2025-11-30 13:48:56 +0800

    Update data path for training model

--- train/train-model.py
+++ train/train-model.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 def main():
     model = YOLO("yolov8n.pt")
     results = model.train(
-        data="data/data.yaml",
+        data="./data/data.yaml",
         epochs=20,
         imgsz=416,
         batch=16,
